b3551346ea673cfefbed8be3f7ebf1ad57c1a940
SebastianSC <sebassandoc@gmail.com>
2024-11-19 19:40:17 -0500

    Modelo usuario y vivienda

--- src/java/modelo/Usuario.java
+++ src/java/modelo/Usuario.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo;
 
-/**
- *
- * @author SEBASTIAN
- */
+import java.sql.Date;
+
 public class Usuario {
+    private int usuarioID;
+    private String nombre;
+    private String apellido;
+    private String usuario;
+    private String clave;
+    private Date fecha_nacimiento;
+    private String genero;
+    private int rol;
     
 }
---
+++ src/java/modelo/Vivienda.java
@@ -0,0 +1,91 @@
+
+package modelo;
+
+public class Vivienda {
+    
+    private int viviendaID;
+    private String tipo;
+    private String ciudad;
+    private String direccion;
+    private String contrato;
+    private float tamanio;
+    private float presupuesto;
+    private String caracteristicas;
+
+    public Vivienda(int viviendaID, String tipo, String ciudad, String direccion, String contrato, float tamanio, float presupuesto, String caracteristicas) {
+        this.viviendaID = viviendaID;
+        this.tipo = tipo;
+        this.ciudad = ciudad;
+        this.direccion = direccion;
+        this.contrato = contrato;
+        this.tamanio = tamanio;
+        this.presupuesto = presupuesto;
+        this.caracteristicas = caracteristicas;
+    }
+
+    public int getViviendaID() {
+        return viviendaID;
+    }
+
+    public void setViviendaID(int viviendaID) {
+        this.viviendaID = viviendaID;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getContrato() {
+        return contrato;
+    }
+
+    public void setContrato(String contrato) {
+        this.contrato = contrato;
+    }
+
+    public float getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(float tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public float getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(float presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+    public String getCaracteristicas() {
+        return caracteristicas;
+    }
+
+    public void setCaracteristicas(String caracteristicas) {
+        this.caracteristicas = caracteristicas;
+    }
+    
+    
+}
